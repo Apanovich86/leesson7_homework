1d3c5c8a9f66de4b997dbb6492c627bdd90b3123
Apanovich86 <apanovich.anna.itec@gmail.com>
2021-08-26 14:19:54 +0300

    lesson7 homework reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {useReducer} from "react";
+import reducer from "./reducers/reducer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    let [state, dispatch] = useReducer(reducer);
+    return (
+        <div>
+            <h1>{state}</h1>
+            <button onClick={() => dispatch('+')}>inc</button>
+            <button onClick={() => dispatch('-')}>dec</button>
+        </div>
+    );
 }
 
-export default App;
+
---
+++ src/reducers/reducer.js
@@ -0,0 +1,11 @@
+const reducer = (state = 0, action) => {
+   switch (action) {
+       case '+':
+           return state + 10;
+       case '-':
+           return state - 2;
+       default:
+           return state;
+   }
+}
+export default reducer;
